d79438416aa0c5ae98f92180af6fe38e1a9f6e90
KennyZ69 <martin.paulik06@gmail.com>
2024-03-20 22:22:39 +0100

    Just cleaned the code a bit and worked a little on the ref

--- src/Referee.tsx
+++ src/Referee.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Position } from './Position';
-import { PieceType } from './Types';
+import { PieceType, Player } from './Types';
+import { Piece } from './Pieces';
 
-export default class Referee{
-    isValidMove(position: Position, destination: Position, type: PieceType){
-        
+export class Referee{
+    isValidMove(position: Position, destination: Position, type: PieceType, team: Player): boolean{
+       console.log('checking for moves');
+       console.log(position);
+       console.log(destination);
+       console.log(team);
+       if(type === PieceType.Pawn){
+
+       }
+        return false;
     }
-}
+};
 
 // export const Referee = () => {
 //   return (
